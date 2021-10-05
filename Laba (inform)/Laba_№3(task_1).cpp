4216ae8630d9059c528990af3b8c6d9f589fd542
Krykov Alexandr <57997351+SoskaVLG@users.noreply.github.com>
2021-10-05 17:04:05 +0300

    Update Laba_№3(task_1).cpp

--- Laba (inform)/Laba_№3(task_1).cpp
+++ Laba (inform)/Laba_№3(task_1).cpp
@@ -1,24 +1,25 @@
+
 #include <iostream>
-using namespace std; 
+using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	float x, n , k , full_product;
+	float x, n, k, full_product;
 
-	cout << "\n Введите число x\n> "; 
-	cin >> x; 
+	cout << "\n Введите число x\n> ";
+	cin >> x;
 	cout << "\nВведите число k \n> ";
-	cin >> k; 
+	cin >> k;
 	full_product = 1;
 
-	for (n = 1; n <= k; n++) 
+	for (n = 1; n <= k; n++)
 	{
-		full_product *= ((n / 2) + x); 
-		cout << "\nПри n = " << n << "\t P = " << full_product; 	
+		full_product *= ((n / 2) + x);
+		cout << "\nПри n = " << n << "\t P = " << full_product << "\n";
 	}
 	cout << "\n";
 
-	system("pause"); 
-	return 0; 
+	system("pause");
+	return 0;
 }
